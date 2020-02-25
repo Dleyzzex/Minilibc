95e4ed266e53c9c90347ca09734adfa2e02ebe2f
Victor Simon <victor.simon@epitech.eu>
2020-02-25 17:39:32 +0100

    FIX - strpbrk

--- src/strpbrk.asm
+++ src/strpbrk.asm
@@ -1,1 +1,26 @@
 BITS 64                         ; 64-bit mode
+GLOBAL strpbrk                  ; export "strpbrk"
+
+SECTION .text
+
+strchr:
+        MOV     RAX, RDI
+
+.loopA:
+        CMP     BYTE[RAX], 0
+        JE      die
+        MOV     R10, RSI
+        JMP     .loopA
+        INC     RAX
+        JMP     .loopB
+
+.loopB:
+        CMP     BYTE[R10], 0
+        JE      .loopA
+        CMP     BYTE[R10], AL
+        JE      die
+        INC     R10
+        JMP     .loopB
+
+die:
+        RET                     ; return RAX
